078afac7183712398f3b4f4cc856b67bbfadd6dd
Ian <iansutherland.az@gmail.com>
2020-06-28 11:45:27 -0700

    added full circulationRepo with tests

--- app.js
+++ app.js
@@ -19,6 +19,40 @@ async function main() {
 
         const limitData = await circulationRepo.get({}, 3);
         assert.equal(limitData.length, 3);
+
+        const id = getData[4]._id.toString();
+        const byId = await circulationRepo.getById(id);
+        assert.deepEqual(byId, getData[4]);
+        const newItem =   {
+            "Newspaper": "WMy Paper",
+            "Daily Circulation, 2004": 1,
+            "Daily Circulation, 2013": 2,
+            "Change in Daily Circulation, 2004-2013": 100,
+            "Pulitzer Prize Winners and Finalists, 1990-2003": 0,
+            "Pulitzer Prize Winners and Finalists, 2004-2014": 0,
+            "Pulitzer Prize Winners and Finalists, 1990-2014": 0
+        };
+        const addedItem = await circulationRepo.add(newItem);
+        assert(addedItem._id);//_id should be added to item if it's there
+        const addedItemQuery = await circulationRepo.getById(addedItem._id);
+        assert.deepEqual(addedItemQuery, newItem);
+
+        const updatedItem = await  circulationRepo.update(addedItem._id, {
+            "Newspaper": "My new paper",
+            "Daily Circulation, 2004": 1,
+            "Daily Circulation, 2013": 2,
+            "Change in Daily Circulation, 2004-2013": 100,
+            "Pulitzer Prize Winners and Finalists, 1990-2003": 0,
+            "Pulitzer Prize Winners and Finalists, 2004-2014": 0,
+            "Pulitzer Prize Winners and Finalists, 1990-2014": 0
+        });
+        const newAddedItemQuery = await circulationRepo.getById(addedItem._id);
+        assert.equal(newAddedItemQuery.Newspaper, "My new paper");
+
+        const removed = await circulationRepo.remove(addedItem._id);
+        assert(removed);
+        const deletedItem = await circulationRepo.getById(addedItem._id);
+        assert.equal(deletedItem, null);
     }
     catch (error){
         console.log(error);
--- repos/circulationRepo.js
+++ repos/circulationRepo.js
@@ -1,4 +1,4 @@
-const {MongoClient} = require('mongodb');
+const {MongoClient, ObjectID} = require('mongodb');
 const {url, dbName} = require('../config.json');
 
 function circulationRepo() {
@@ -31,7 +31,7 @@ function circulationRepo() {
                 if(limit > 0){
                     items = items.limit(limit);
                 }
-                
+
                 resolve(await items.toArray());
                 client.close();
             }
@@ -41,7 +41,77 @@ function circulationRepo() {
         })
     }
 
-    return {loadData, get}
+    function getById(id){
+        return new Promise(async (resolve, reject) => {
+            const client = new MongoClient(url);
+            try{
+                await client.connect();
+                const db = client.db(dbName);
+
+                const item = await db.collection('newspapers').findOne({_id: ObjectID(id)});
+
+                resolve(item);
+                client.close();
+            }
+            catch (error){
+                reject(error);
+            }
+        })
+    }
+
+    function add(item){
+        return new Promise(async (resolve, reject) => {
+            const client = new MongoClient(url);
+            try{
+                await client.connect();
+                const db = client.db(dbName);
+
+                const addedItem = await db.collection('newspapers').insertOne(item);
+
+                resolve(addedItem.ops[0]);
+                client.close();
+            }
+            catch (error){
+                reject(error);
+            }
+        })
+    }
+
+    function update(id, newItem){
+        return new Promise(async (resolve, reject) => {
+            const client = new MongoClient(url);
+            try{
+                await client.connect();
+                const db = client.db(dbName);
+                const updatedItem = await db.collection('newspapers')
+                .findOneAndReplace({_id: ObjectID(id)}, newItem, {returnOriginal:false});
+
+                resolve(updatedItem.value);
+                client.close();
+            }
+            catch (error){
+                reject(error);
+            }
+        })
+    }
+
+    function remove(id){
+        return new Promise(async (resolve, reject) => {
+            const client = new MongoClient(url);
+            try{
+                await client.connect();
+                const db = client.db(dbName);
+                const removed = db.collection('newspapers').deleteOne({_id: ObjectID(id)});
+                resolve(removed);
+                client.close();
+            }
+            catch (error){
+                reject(error);
+            }
+        })
+    }
+
+    return {loadData, get, getById, add, update, remove}
 }
 
 module.exports = circulationRepo();
